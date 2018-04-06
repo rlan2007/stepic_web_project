b6c0dda9e643dc09dddceba627edb0517ce6400d
rlan <rlan2007@yandex.ru>
2018-04-06 19:49:37 +0000

    Task 1.8.12v1

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+#!/bin//bash
+sudo rm /etc/nginx/sites-enabled/*
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
