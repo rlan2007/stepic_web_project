cc933188a73f48aa371f093b7994f3b241dfcf90
rlan2007 <rlan2007@yandex.ru>
2018-04-08 22:04:59 +0000

    Task 1.8.12v1

--- gitconf.sh
+++ gitconf.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
-git config --global user.name "rlan2007"
-git config --global user.email "rlan2007@yandex.ru"
+#! /bin/bash 
+git config --global user.name "rlan2007" 
+git config --global user.email "rlan2007@yandex.ru" 
 
--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
-#!/bin//bash
-sudo rm /etc/nginx/sites-enabled/default
+#! /bin/bash
+sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
+#sudo /etc/init.d/nginx restart
+sudo service nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
